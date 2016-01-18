616e866d09b0d6a25e6ecbb1ab0c4ad55d0da8c1
Rachel (Rui Liu) <rachelliu1020@gmail.com>
2016-01-18 00:10:58 -0800

    remote1

--- src/Pet.java
+++ src/Pet.java
@@ -13,7 +13,7 @@ public class Pet {
     private String species;     // Species of this pet (dog, cat, etc)
     private String name;        // Name of this pet
     private int age;            // Age of this pet, in years
-
+private remote1;
 
     //-----------------------------------------------------------------
     //      C o n s t r u c t o r s
