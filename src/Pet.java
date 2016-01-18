8686addc12901f02c4eea631b91d2ab6cf0c5dda
Rachel (Rui Liu) <rachelliu1020@gmail.com>
2016-01-17 20:10:41 -0800

    change by github

--- src/Pet.java
+++ src/Pet.java
@@ -13,9 +13,7 @@ public class Pet {
     private String species;     // Species of this pet (dog, cat, etc)
     private String name;        // Name of this pet
     private int age;            // Age of this pet, in years
-    private int testSAP;
-    private int change;
-    private int trymerge;
+    private int github;
 
     //-----------------------------------------------------------------
     //      C o n s t r u c t o r s
